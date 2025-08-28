17801a4fd542ecfed3eaa97035baddb0e01f4d06
vktrrdk <accounts@viktorrudko.de>
2025-08-28 13:35:35 +0000

    trying stuff

--- run_docker.sh
+++ run_docker.sh
@@ -1,10 +1,9 @@
-#!bin/bash
+#!/bin/bash
 
-cd /src/jekyll
+cd /srv/jekyll
 
 git remote set-url origin https://github.com/deKCD/Training-Materials.git
 
-cd
 
 bundle exec ruby webhook_server.rb & bundle exec jekyll serve --trace -H 0.0.0.0
 
