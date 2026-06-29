41ea2d123d40c2fb4dd8d5401ea2c8090dd47b3c
Sebastian Juenemann <jueneman@cebitec.uni-bielefeld.de>
2026-06-29 14:52:15 +0200

    hotfix to the run_docker.sh for the jekyll serve

--- run_docker.sh
+++ run_docker.sh
@@ -5,7 +5,8 @@ cd /srv/jekyll
 git remote set-url origin https://github.com/deKCD/Training-Materials.git
 
 
-bundle exec ruby webhook_server.rb & bundle exec jekyll build JEKYLL_ENV=production
+bundle exec ruby webhook_server.rb & bundle exec jekyll serve --host 0.0.0.0 --port 4000 
+#bundle exec ruby webhook_server.rb & bundle exec jekyll build JEKYLL_ENV=production
 
 wait
 
