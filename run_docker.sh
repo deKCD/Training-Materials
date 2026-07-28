506a468cbb258652dea94fd9bb19e0651fb1a045
vktrrdk <accounts@viktorrudko.de>
2026-07-28 13:31:08 +0000

    check for correct url

--- run_docker.sh
+++ run_docker.sh
@@ -5,7 +5,7 @@ cd /srv/jekyll
 git remote set-url origin https://github.com/deKCD/Training-Materials.git
 
 
-bundle exec ruby webhook_server.rb & bundle exec jekyll serve --host 0.0.0.0 --port 4000 
+bundle exec ruby webhook_server.rb & bundle exec jekyll serve --host 0.0.0.0 --port 4000 --url ${SITE_URL:-https://datenkompetenz.cloud} 
 #bundle exec ruby webhook_server.rb & bundle exec jekyll build JEKYLL_ENV=production
 
 wait
